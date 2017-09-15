6d79f9547a7e938167342098408d24510b7e39f7
TosZira <31985509+TosZira@users.noreply.github.com>
2017-09-15 17:11:14 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,7 +14,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			//$text = $event['message']['text'];
-			$text = $event['message']['ทดสอบ'];
+			$text = $event['message']['test'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
