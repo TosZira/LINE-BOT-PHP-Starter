69048ff2c07f186f7a9b2889471af735c81e557a
TosZira <31985509+TosZira@users.noreply.github.com>
2017-09-15 17:03:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,8 +19,8 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
-				'text' => $123
+				'type' => '123',
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
