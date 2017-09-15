3c1f0b5dca1caf5fec6e8fe71fe87442aa192600
TosZira <31985509+TosZira@users.noreply.github.com>
2017-09-15 17:05:14 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,13 +13,14 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text'];
+			//$text = $event['message']['text'];
+			$text = '1234';
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
 			$messages = [
-				'type' => '123',
+				'type' => 'text',
 				'text' => $text
 			];
 
